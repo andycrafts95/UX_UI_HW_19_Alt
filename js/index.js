fbabd204d95302e7c0394e9ba0a9f79699d591d0
andycrafts95 <88752407+andycrafts95@users.noreply.github.com>
2022-02-10 15:13:01 -0500

    animated buttons

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // index.js
-console.log ("Your index.js file is loaded correctly!");
+console.log ("Your index.js file is loaded correctly");
 //HW 19 JQuery Listeners
-$("#About").on("click", function (){
-    
-})
+//$("#About").on("click", function (){
+  //  console.log ("you clicked about");
+//});
+
+$("#downloadResume").on("click", function(){
+    console.log("download resume");
+});
