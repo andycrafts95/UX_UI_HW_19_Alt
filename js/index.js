32055414f77a0ff6d04754678e1e04b6d7cf2161
andycrafts95 <88752407+andycrafts95@users.noreply.github.com>
2022-02-10 15:26:02 -0500

    added download resume modal

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 // index.js
 console.log ("Your index.js file is loaded correctly");
 //HW 19 JQuery Listeners
-//$("#About").on("click", function (){
-  //  console.log ("you clicked about");
-//});
+// Get the modal
+var modal = document.getElementById("myModal");
 
-$("#downloadResume").on("click", function(){
-    console.log("download resume");
-});
+// Get the button that opens the modal
+var btn = document.getElementById("downloadResume");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
